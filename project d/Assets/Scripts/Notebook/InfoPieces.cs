ca2b47cb9369f44213dd6676172e3ce64a6cfa74
SunniJB <nepimpeta@gmail.com>
2023-12-01 19:15:18 -0600

    Struggling with click and drag

--- project d/Assets/Scripts/Notebook/InfoPieces.cs
+++ project d/Assets/Scripts/Notebook/InfoPieces.cs
@@ -1,54 +1,77 @@
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
+using System.Diagnostics;
 
 public class InfoPieces : MonoBehaviour
 {
     private Vector3 worldPosition, startPosition, endPosition;
-    public Image thisImage;
     public bool touchedABox;
+    public GameObject currentBox;
 
     private void Start()
     {
-        thisImage = GetComponent<Image>();
-        startPosition = gameObject.transform.position;
+        startPosition = transform.position;
     }
     void Update()
     {
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = 7.19f;
         worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                UnityEngine.Debug.Log(hit.collider.gameObject.name);
+            }
+        }
     }
     public void OnDrag()
     {
-        thisImage.transform.position = worldPosition;
+        transform.position = worldPosition;
+        UnityEngine.Debug.Log("Dragging should occur");
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        UnityEngine.Debug.Log("Entered a collider");
         if (other.gameObject.CompareTag("box"))
         {
+            UnityEngine.Debug.Log("Entered the box's collider");
+            currentBox = other.gameObject;
             touchedABox = true;
             endPosition = other.gameObject.transform.position;
+            other.gameObject.GetComponent<Image>().enabled = false;
+            other.gameObject.GetComponentInChildren<TextMeshProUGUI>().text = "";
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
+        UnityEngine.Debug.Log("Left a collider");
         if (other.gameObject.CompareTag("box"))
         {
+            UnityEngine.Debug.Log("Left the box's collider");
             touchedABox = false;
-            endPosition = startPosition;
+            currentBox = null;
+            other.gameObject.GetComponent<Image>().enabled = true;
+            other.gameObject.GetComponentInChildren<TextMeshProUGUI>().text = "Drag information here";
         }
     }
 
     public void EndDrag()
     {
+        UnityEngine.Debug.Log("End Drag fired");
         if (touchedABox)
         {
+            UnityEngine.Debug.Log("Ended drag, touched a box");
             transform.position = endPosition;
         } else
         {
+            UnityEngine.Debug.Log("Ended drag, didn't touch a box");
             transform.position = startPosition;
         }
     }
--- project d/Assets/Scripts/Notebook/Notebook.cs
+++ project d/Assets/Scripts/Notebook/Notebook.cs
@@ -2,19 +2,12 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Notebook : MonoBehaviour, IDragHandler
+public class Notebook : MonoBehaviour
 {
     [SerializeField] GameManager gameManager;
-    [SerializeField] GameObject[] infoPieces;
 
     private void Start()
     {
         gameManager = GameManager.Instance;
     }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        transform.position = eventData.position;
-    }
- 
 }
